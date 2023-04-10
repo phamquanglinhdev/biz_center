b10c94669c5b66f01b7805fdaf4efe9aaf93943d
phamquanglinhdev <phamquanglinhdev@gmail.com>
2023-04-11 06:27:18 +0700

    Fix avatar

--- resources/views/layouts/inc/avatar.blade.php
+++ resources/views/layouts/inc/avatar.blade.php
@@ -1,3 +1,6 @@
+<div class="mt-4 mt-md-0">
+    <img class="rounded-circle avatar-xl" id="preview-{{$name}}" alt="200x200" src="" data-holder-rendered="true">
+</div>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropper/4.1.0/cropper.css"/>
 <style>
     .cropper-modal img {
@@ -15,7 +18,7 @@
 </style>
 <div class="input-group">
     <label class="input-group-text" for="inputGroupFile01">Tải lên</label>
-    <input type="file" id="{{$name}}" name="{{$name}}" class="form-control cropper-image">
+    <input type="file" id="upload-{{$name}}" name="{{$name}}" class="form-control cropper-image">
 </div>
 
 <!-- Default Modals -->
@@ -24,7 +27,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="myModalLabel">Cắt ảnh</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"> </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="img-container">
@@ -49,66 +52,61 @@
     <script src="https://unpkg.com/jquery@3/dist/jquery.slim.min.js"></script>
     <script src="https://fengyuanchen.github.io/cropperjs/js/cropper.js"></script>
     <script>
-        var bs_modal = $('#modal');
-        var image = document.getElementById('{{$name}}');
-        var cropper, reader, file;
-        $("body").on("change", ".cropper-image", function (e) {
-            const files = e.target.files;
-            const done = function (url) {
-                image.src = url;
-                bs_modal.modal('show');
-            };
-            if (files && files.length > 0) {
-                file = files[0];
-                if (URL) {
-                    done(URL.createObjectURL(file));
-                } else if (FileReader) {
-                    reader = new FileReader();
-                    reader.onload = function (e) {
-                        console.log(e)
-                    };
-                    reader.readAsDataURL(file);
-                }
-            }
-        });
+        $(document).ready(function () {
+            let bs_modal = $('#modal');
+            let image = document.getElementById('{{$name}}');
+            let cropper, reader, file;
 
-        bs_modal.on('shown.bs.modal', function () {
-            cropper = new Cropper(image, {
-                aspectRatio: 1,
-                viewMode: 3,
-                preview: '.preview'
-            });
-        }).on('hidden.bs.modal', function () {
-            cropper.destroy();
-            cropper = null;
-        });
-
-        $("#crop").click(function () {
-            canvas = cropper.getCroppedCanvas({
-                width: 160,
-                height: 160,
-            });
-
-            canvas.toBlob(function (blob) {
-                url = URL.createObjectURL(blob);
-                var reader = new FileReader();
-                reader.readAsDataURL(blob);
-                reader.onloadend = function () {
-                    var base64data = reader.result;
-
-                    $.ajax({
-                        type: "POST",
-                        dataType: "json",
-                        url: "upload.php",
-                        data: {image: base64data},
-                        success: function (data) {
-                            bs_modal.modal('hide');
-                            alert("success upload image");
-                        }
-                    });
+            $("body").on("change", ".cropper-image", function (e) {
+                let files = e.target.files;
+                const done = function (url) {
+                    image.src = url;
+                    bs_modal.modal('show');
                 };
+                if (files && files.length > 0) {
+                    file = files[0];
+                    if (URL) {
+                        done(URL.createObjectURL(file));
+                    } else if (FileReader) {
+                        reader = new FileReader();
+                        reader.onload = function (e) {
+                            console.log(e)
+                        };
+                        reader.readAsDataURL(file);
+                    }
+                }
             });
-        });
+
+            bs_modal.on('shown.bs.modal', function () {
+                cropper = new Cropper(image, {
+                    aspectRatio: 1,
+                    viewMode: 2,
+                    preview: '.preview'
+                });
+            }).on('hidden.bs.modal', function () {
+                cropper.destroy();
+                cropper = null;
+            });
+
+            $("#crop").click(function () {
+                const canvas = cropper.getCroppedCanvas({
+                    width: 200,
+                    height: 200,
+                });
+                canvas.toBlob(function (blob) {
+                    let url = URL.createObjectURL(blob);
+                    let reader = new FileReader();
+                    reader.readAsDataURL(blob);
+                    reader.onloadend = function () {
+                        let base64data = reader.result;
+                        console.log(base64data)
+                        document.getElementById("preview-{{$name}}").src=base64data
+                    };
+                });
+
+                bs_modal.modal('hide');
+            });
+        })
 
     </script>
 @endpush
